5e4831b4663e4ab0f5f72018659487b71f9d6a2a
Bruno Aguirre <aguirre@gapso.com.br>
2010-08-19 19:51:22 +0000

    Adding Persistence Dependency Injection.

--- code/client/src/main/java/com/gapso/web/trieda/server/CampiServiceImpl.java
+++ code/client/src/main/java/com/gapso/web/trieda/server/CampiServiceImpl.java
@@ -1,11 +1,16 @@
 package com.gapso.web.trieda.server;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import com.extjs.gxt.ui.client.data.BasePagingLoadResult;
 import com.extjs.gxt.ui.client.data.ModelData;
 import com.extjs.gxt.ui.client.data.PagingLoadResult;
+import com.gapso.trieda.domain.Campus;
 import com.gapso.web.trieda.client.mvc.model.CampusModel;
 import com.gapso.web.trieda.client.services.CampiService;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
@@ -18,7 +23,12 @@ public class CampiServiceImpl extends RemoteServiceServlet implements CampiServi
 	private static final long serialVersionUID = 5250776996542788849L;
 
 	public PagingLoadResult<ModelData> getList() {
+		
+		WebApplicationContext ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
+		Campus bean = ctx.getBean("campusBean", Campus.class);
+		
 		List<ModelData> list = new ArrayList<ModelData>();
+		list.add(new CampusModel("bean " + bean, "********"));
 		for(int i = 1; i <= 10; i++) {
 			list.add(new CampusModel("Codigo "+i, "Nome "+i));
 		}
