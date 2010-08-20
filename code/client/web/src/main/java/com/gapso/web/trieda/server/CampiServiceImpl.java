eb841c990a55c20808dad78e4e21848169a2a4e5
Claudio Escudero <claudio@gapso.com.br>
2010-08-20 14:28:54 +0000

    Ajustando a persistencia de endereços com o roo

--- code/client/web/src/main/java/com/gapso/web/trieda/server/CampiServiceImpl.java
+++ code/client/web/src/main/java/com/gapso/web/trieda/server/CampiServiceImpl.java
@@ -1,16 +1,12 @@
 package com.gapso.web.trieda.server;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import com.extjs.gxt.ui.client.data.BasePagingLoadResult;
 import com.extjs.gxt.ui.client.data.ModelData;
 import com.extjs.gxt.ui.client.data.PagingLoadResult;
-import com.gapso.trieda.domain.Campus;
+import com.gapso.trieda.domain.Endereco;
 import com.gapso.web.trieda.client.mvc.model.CampusModel;
 import com.gapso.web.trieda.client.services.CampiService;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
@@ -24,13 +20,10 @@ public class CampiServiceImpl extends RemoteServiceServlet implements CampiServi
 
 	public PagingLoadResult<ModelData> getList() {
 		
-		WebApplicationContext ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
-		Campus bean = ctx.getBean("campusBean", Campus.class);
-		
 		List<ModelData> list = new ArrayList<ModelData>();
-		list.add(new CampusModel("bean " + bean, "********"));
-		for(int i = 1; i <= 10; i++) {
-			list.add(new CampusModel("Codigo "+i, "Nome "+i));
+		
+		for(Endereco c : Endereco.findAllEnderecoes()) {
+			list.add(new CampusModel(c.getCidade(), c.getLogradouro()));
 		}
 		return new BasePagingLoadResult<ModelData>(list);
 	}
--- code/client/web/src/main/java/com/gapso/web/trieda/server/ProfessoresServiceImpl.java
+++ code/client/web/src/main/java/com/gapso/web/trieda/server/ProfessoresServiceImpl.java
@@ -3,9 +3,14 @@ package com.gapso.web.trieda.server;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
 import com.extjs.gxt.ui.client.data.BasePagingLoadResult;
 import com.extjs.gxt.ui.client.data.ModelData;
 import com.extjs.gxt.ui.client.data.PagingLoadResult;
+import com.gapso.trieda.domain.Endereco;
+import com.gapso.trieda.misc.Estados;
 import com.gapso.web.trieda.client.mvc.model.ProfessorModel;
 import com.gapso.web.trieda.client.services.ProfessoresService;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
@@ -18,6 +23,18 @@ public class ProfessoresServiceImpl extends RemoteServiceServlet implements Prof
 	private static final long serialVersionUID = 5250776996542788849L;
 
 	public PagingLoadResult<ModelData> getList() {
+		
+		WebApplicationContext ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
+		
+		for(int i = 1; i <= 10; i++) {
+//			Endereco bean = ctx.getBean("enderecoBean", Endereco.class);
+			Endereco bean = new Endereco();
+			bean.setCidade("S Cidade "+i);
+			bean.setEstado(Estados.RJ);
+			bean.setLogradouro("S Logradouro "+i);
+			bean.persist();
+		}
+		
 		List<ModelData> list = new ArrayList<ModelData>();
 		for(int i = 1; i <= 10; i++) {
 			list.add(new ProfessorModel("111.222.333-44", "Nome "+i, "Tipo "+i, i, i, "Titulacao "+i, "Area de Titulacao "+i, "Boa", i, i));
