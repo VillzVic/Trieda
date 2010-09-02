79a06cc6af1a07993898a94db536e30ec3152ba2
Claudio Escudero <claudio@gapso.com.br>
2010-09-02 22:57:40 +0000

    TRIEDA-150 Criando a busca de turnos na entidade

--- code/client/domain/src/main/java/com/gapso/trieda/domain/Turno.java
+++ code/client/domain/src/main/java/com/gapso/trieda/domain/Turno.java
@@ -2,7 +2,6 @@ package com.gapso.trieda.domain;
 
 import java.util.List;
 import java.util.Set;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,13 +11,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.persistence.Table;
 import javax.persistence.Version;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.roo.addon.entity.RooEntity;
 import org.springframework.roo.addon.javabean.RooJavaBean;
@@ -48,41 +47,41 @@ public class Turno implements java.io.Serializable {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "turno")
     private Set<com.gapso.trieda.domain.CampusCurriculo> campusCurriculo = new java.util.HashSet<com.gapso.trieda.domain.CampusCurriculo>();
 
-	@PersistenceContext
+    @PersistenceContext
     transient EntityManager entityManager;
 
-	@Id
+    @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "TUR_ID")
     private Long id;
 
-	@Version
+    @Version
     @Column(name = "version")
     private Integer version;
 
-	public Long getId() {
+    public Long getId() {
         return this.id;
     }
 
-	public void setId(Long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-	public Integer getVersion() {
+    public Integer getVersion() {
         return this.version;
     }
 
-	public void setVersion(Integer version) {
+    public void setVersion(Integer version) {
         this.version = version;
     }
 
-	@Transactional
+    @Transactional
     public void persist() {
         if (this.entityManager == null) this.entityManager = entityManager();
         this.entityManager.persist(this);
     }
 
-	@Transactional
+    @Transactional
     public void remove() {
         if (this.entityManager == null) this.entityManager = entityManager();
         if (this.entityManager.contains(this)) {
@@ -93,13 +92,13 @@ public class Turno implements java.io.Serializable {
         }
     }
 
-	@Transactional
+    @Transactional
     public void flush() {
         if (this.entityManager == null) this.entityManager = entityManager();
         this.entityManager.flush();
     }
 
-	@Transactional
+    @Transactional
     public Turno merge() {
         if (this.entityManager == null) this.entityManager = entityManager();
         Turno merged = this.entityManager.merge(this);
@@ -107,62 +106,81 @@ public class Turno implements java.io.Serializable {
         return merged;
     }
 
-	public static final EntityManager entityManager() {
+    public static final EntityManager entityManager() {
         EntityManager em = new Turno().entityManager;
         if (em == null) throw new IllegalStateException("Entity manager has not been injected (is the Spring Aspects JAR configured as an AJC/AJDT aspects library?)");
         return em;
     }
 
-	public static int count() {
+    public static int count() {
         return ((Number) entityManager().createQuery("select count(o) from Turno o").getSingleResult()).intValue();
     }
 
-	@SuppressWarnings("unchecked")
+    @SuppressWarnings("unchecked")
     public static List<Turno> findAll() {
         return entityManager().createQuery("select o from Turno o").getResultList();
     }
 
-	public static Turno find(Long id) {
+    public static Turno find(Long id) {
         if (id == null) return null;
         return entityManager().find(Turno.class, id);
     }
 
-	public static List<Turno> find(int firstResult, int maxResults) {
-		return find(firstResult, maxResults, null);
-	}
-	@SuppressWarnings("unchecked")
-    public static List<Turno> find(int firstResult, int maxResults, String orderBy) {
-		orderBy = (orderBy != null)? "ORDER BY o."+orderBy : "";
-        return entityManager().createQuery("SELECT o FROM Turno o "+orderBy).setFirstResult(firstResult).setMaxResults(maxResults).getResultList();
+    public static List<Turno> find(int firstResult, int maxResults) {
+        return find(firstResult, maxResults, null);
     }
 
-	public String getNome() {
+    @SuppressWarnings("unchecked")
+    public static List<Turno> find(int firstResult, int maxResults, String orderBy) {
+        orderBy = (orderBy != null) ? "ORDER BY o." + orderBy : "";
+        return entityManager().createQuery("SELECT o FROM Turno o " + orderBy).setFirstResult(firstResult).setMaxResults(maxResults).getResultList();
+    }
+
+    public static Query findByNomeLikeAndTempo(String nome, Integer tempo) {
+    	nome = (nome == null || nome.length() == 0)? "" : nome;
+        nome = nome.replace('*', '%');
+        if (nome.charAt(0) != '%') {
+            nome = "%" + nome;
+        }
+        if (nome.charAt(nome.length() -1) != '%') {
+            nome = nome + "%";
+        }
+        
+        EntityManager em = Turno.entityManager();
+        String queryTempo = (tempo != null)? "AND turno.tempo = :tempo" : "";
+        Query q = em.createQuery("SELECT Turno FROM Turno AS turno WHERE LOWER(turno.nome) LIKE LOWER(:nome) "+queryTempo);
+        q.setParameter("nome", nome);
+        if(tempo != null) q.setParameter("tempo", tempo);
+        return q;
+    }
+    
+    public String getNome() {
         return this.nome;
     }
 
-	public void setNome(String nome) {
+    public void setNome(String nome) {
         this.nome = nome;
     }
 
-	public Integer getTempo() {
+    public Integer getTempo() {
         return this.tempo;
     }
 
-	public void setTempo(Integer tempo) {
+    public void setTempo(Integer tempo) {
         this.tempo = tempo;
     }
 
-	public Set<CampusCurriculo> getCampusCurriculo() {
+    public Set<CampusCurriculo> getCampusCurriculo() {
         return this.campusCurriculo;
     }
 
-	public void setCampusCurriculo(Set<CampusCurriculo> campusCurriculo) {
+    public void setCampusCurriculo(Set<CampusCurriculo> campusCurriculo) {
         this.campusCurriculo = campusCurriculo;
     }
 
-	private static final long serialVersionUID = 2608398950191790873L;
+    private static final long serialVersionUID = 2608398950191790873L;
 
-	public String toString() {
+    public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Id: ").append(getId()).append(", ");
         sb.append("Version: ").append(getVersion()).append(", ");
