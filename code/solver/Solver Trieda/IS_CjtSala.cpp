4bcca08f254d39e3a07cd8e3379050590fa36576
Mário Perché <mariohpp@gapso.com.br>
2010-12-20 11:32:45 +0000

    Adicionando IS_CjtSala.h e IS_CjtSala.cpp

---
+++ code/solver/Solver Trieda/IS_CjtSala.cpp
@@ -0,0 +1,13 @@
+#include "IS_CjtSala.h"
+
+IS_CjtSala::IS_CjtSala(void)
+{
+}
+
+IS_CjtSala::IS_CjtSala(IS_CjtSala const & is_cjtSala)
+{
+}
+
+IS_CjtSala::~IS_CjtSala(void)
+{
+}
---
+++ code/solver/Solver Trieda/IS_CjtSala.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class IS_CjtSala
+{
+public:
+   IS_CjtSala(void);
+
+   IS_CjtSala(IS_CjtSala const & is_cjtSala);
+
+   virtual ~IS_CjtSala(void);
+
+private:
+   
+};
