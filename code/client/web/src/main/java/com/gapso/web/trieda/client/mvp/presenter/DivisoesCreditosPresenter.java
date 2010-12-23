3f05ba4befd9556c7dc2ac324ddb2beb87e8e632
Claudio Escudero <claudio@gapso.com.br>
2010-12-23 10:57:38 +0000

    TRIEDA-504 TRIEDA-505 TRIEDA-506 Criando as telas de Divisão de Créditos

---
+++ code/client/web/src/main/java/com/gapso/web/trieda/client/mvp/presenter/DivisoesCreditosPresenter.java
@@ -0,0 +1,123 @@
+package com.gapso.web.trieda.client.mvp.presenter;
+
+import java.util.List;
+
+import com.extjs.gxt.ui.client.data.PagingLoadConfig;
+import com.extjs.gxt.ui.client.data.PagingLoadResult;
+import com.extjs.gxt.ui.client.data.RpcProxy;
+import com.extjs.gxt.ui.client.event.ButtonEvent;
+import com.extjs.gxt.ui.client.event.SelectionListener;
+import com.extjs.gxt.ui.client.widget.Component;
+import com.extjs.gxt.ui.client.widget.Info;
+import com.extjs.gxt.ui.client.widget.MessageBox;
+import com.extjs.gxt.ui.client.widget.button.Button;
+import com.extjs.gxt.ui.client.widget.form.NumberField;
+import com.extjs.gxt.ui.client.widget.form.TextField;
+import com.gapso.web.trieda.client.mvp.model.CenarioDTO;
+import com.gapso.web.trieda.client.mvp.model.TurnoDTO;
+import com.gapso.web.trieda.client.mvp.view.TurnoFormView;
+import com.gapso.web.trieda.client.services.Services;
+import com.gapso.web.trieda.client.services.TurnosServiceAsync;
+import com.gapso.web.trieda.client.util.view.GTab;
+import com.gapso.web.trieda.client.util.view.GTabItem;
+import com.gapso.web.trieda.client.util.view.SimpleGrid;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Widget;
+
+public class DivisoesCreditosPresenter implements Presenter {
+
+	public interface Display {
+		Button getNewButton();
+		Button getEditButton();
+		Button getRemoveButton();
+		Button getImportExcelButton();
+		Button getExportExcelButton();
+		TextField<String> getNomeBuscaTextField();
+		NumberField getTempoBuscaTextField();
+		Button getSubmitBuscaButton();
+		Button getResetBuscaButton();
+		SimpleGrid<TurnoDTO> getGrid();
+		Component getComponent();
+		void setProxy(RpcProxy<PagingLoadResult<TurnoDTO>> proxy);
+	}
+	private CenarioDTO cenario;
+	private Display display; 
+	
+	public DivisoesCreditosPresenter(CenarioDTO cenario, Display display) {
+		this.cenario = cenario;
+		this.display = display;
+		configureProxy();
+		setListeners();
+	}
+
+	private void configureProxy() {
+		final TurnosServiceAsync service = Services.turnos();
+		RpcProxy<PagingLoadResult<TurnoDTO>> proxy = new RpcProxy<PagingLoadResult<TurnoDTO>>() {
+			@Override
+			public void load(Object loadConfig, AsyncCallback<PagingLoadResult<TurnoDTO>> callback) {
+//				service.getList((PagingLoadConfig)loadConfig, callback);
+				String nome = display.getNomeBuscaTextField().getValue();
+				Number tempo = display.getTempoBuscaTextField().getValue();
+				service.getBuscaList(nome, (tempo==null)?null:tempo.intValue(), (PagingLoadConfig)loadConfig, callback);
+			}
+		};
+		display.setProxy(proxy);
+	}
+	
+	private void setListeners() {
+		display.getNewButton().addSelectionListener(new SelectionListener<ButtonEvent>() {
+			@Override
+			public void componentSelected(ButtonEvent ce) {
+				Presenter presenter = new TurnoFormPresenter(cenario, new TurnoFormView(new TurnoDTO()), display.getGrid());
+				presenter.go(null);
+			}
+		});
+		display.getEditButton().addSelectionListener(new SelectionListener<ButtonEvent>() {
+			@Override
+			public void componentSelected(ButtonEvent ce) {
+				TurnoDTO turnoDTO = display.getGrid().getGrid().getSelectionModel().getSelectedItem();
+				Presenter presenter = new TurnoFormPresenter(cenario, new TurnoFormView(turnoDTO), display.getGrid());
+				presenter.go(null);
+			}
+		});
+		display.getRemoveButton().addSelectionListener(new SelectionListener<ButtonEvent>(){
+			@Override
+			public void componentSelected(ButtonEvent ce) {
+				List<TurnoDTO> list = display.getGrid().getGrid().getSelectionModel().getSelectedItems();
+				final TurnosServiceAsync service = Services.turnos();
+				service.remove(list, new AsyncCallback<Void>() {
+					@Override
+					public void onFailure(Throwable caught) {
+						MessageBox.alert("ERRO!", "Deu falha na conexão", null);
+					}
+					@Override
+					public void onSuccess(Void result) {
+						display.getGrid().updateList();
+						Info.display("Removido", "Item removido com sucesso!");
+					}
+				});
+			}
+		});
+		display.getResetBuscaButton().addSelectionListener(new SelectionListener<ButtonEvent>(){
+			@Override
+			public void componentSelected(ButtonEvent ce) {
+				display.getNomeBuscaTextField().setValue(null);
+				display.getTempoBuscaTextField().setValue(null);
+				display.getGrid().updateList();
+			}
+		});
+		display.getSubmitBuscaButton().addSelectionListener(new SelectionListener<ButtonEvent>(){
+			@Override
+			public void componentSelected(ButtonEvent ce) {
+				display.getGrid().updateList();
+			}
+		});
+	}
+	
+	@Override
+	public void go(Widget widget) {
+		GTab tab = (GTab)widget;
+		tab.add((GTabItem)display.getComponent());
+	}
+
+}
---
+++ code/client/web/src/main/java/com/gapso/web/trieda/client/services/DivisoesCreditosService.java
@@ -0,0 +1,29 @@
+package com.gapso.web.trieda.client.services;
+
+import java.util.List;
+
+import com.extjs.gxt.ui.client.data.BasePagingLoadConfig;
+import com.extjs.gxt.ui.client.data.ListLoadResult;
+import com.extjs.gxt.ui.client.data.PagingLoadConfig;
+import com.extjs.gxt.ui.client.data.PagingLoadResult;
+import com.gapso.web.trieda.client.mvp.model.CampusDTO;
+import com.gapso.web.trieda.client.mvp.model.TurnoDTO;
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+/**
+ * The client side stub for the RPC service.
+ */
+@RemoteServiceRelativePath("divisoesCreditos")
+public interface DivisoesCreditosService extends RemoteService {
+	
+	TurnoDTO getTurno(Long id);
+	PagingLoadResult<TurnoDTO> getList(PagingLoadConfig config);
+	ListLoadResult<TurnoDTO> getList();
+	ListLoadResult<TurnoDTO> getList(BasePagingLoadConfig loadConfig);
+	void save(TurnoDTO turnoDTO);
+	void remove(List<TurnoDTO> turnoDTOList);
+	PagingLoadResult<TurnoDTO> getBuscaList(String nome, Integer tempo, PagingLoadConfig config);
+	ListLoadResult<TurnoDTO> getListByCampus(CampusDTO campusDTO);
+	
+}
