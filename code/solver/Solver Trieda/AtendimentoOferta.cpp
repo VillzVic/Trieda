503adcecff1ca617d109372e272c2e6d615de649
Marcelo Reis <marcelo@gapso.com.br>
2011-06-06 14:46:49 +0000

    - Correção para imprimir sempre o id da disciplina positivo no output.

--- code/solver/Solver Trieda/AtendimentoOferta.cpp
+++ code/solver/Solver Trieda/AtendimentoOferta.cpp
@@ -17,7 +17,7 @@ AtendimentoOferta::~AtendimentoOferta( void )
 std::ostream & operator << ( std::ostream & out, AtendimentoOferta & oferta )
 {
    out << "<ofertaCursoCampiId>" << oferta.getOfertaCursoCampiId() << "</ofertaCursoCampiId>" << std::endl;
-   out << "<disciplinaId>" << oferta.getDisciplinaId() << "</disciplinaId>" << std::endl;
+   out << "<disciplinaId>" << abs(oferta.getDisciplinaId()) << "</disciplinaId>" << std::endl;
    out << "<quantidade>" << oferta.getQuantidade() << "</quantidade>" << std::endl;
    out << "<turma>" << oferta.getTurma() << "</turma>" << std::endl;
 
