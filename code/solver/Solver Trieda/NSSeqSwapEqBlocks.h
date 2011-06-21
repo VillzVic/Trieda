c7653d9c2c034c0e912176b1e39508aa74cc3845
Cleiton Mendes Dias <cleiton@gapso.com.br>
2011-06-21 12:51:28 +0000

    Escrever o output do modulo operacional

--- code/solver/Solver Trieda/NSSeqSwapEqBlocks.h
+++ code/solver/Solver Trieda/NSSeqSwapEqBlocks.h
@@ -3,35 +3,26 @@
 #define _NS_SEQ_SWAP_EQ_BLOCKS_H_
 
 #include "NSSeq.hpp"
-
 #include "MoveSwap.h"
-
 #include "MoveSwapEqBlocksValidator.h"
-
 #include "NSIteratorSwapEqBlocks.h"
-
 #include "ProblemData.h"
 
-class NSSeqSwapEqBlocks : public NSSeq
+class NSSeqSwapEqBlocks
+   : public NSSeq
 {
-private:
-   ProblemData & problemData;
-
-   std::pair<Aula*,Aula*> pickTwoClasses(SolucaoOperacional& s);
-
-   MoveSwapEqBlocksValidator * moveValidator;
-
 public:
-   NSSeqSwapEqBlocks(ProblemData & problemData);
-
+   NSSeqSwapEqBlocks( ProblemData & );
    virtual ~NSSeqSwapEqBlocks();
 
-   Move & move(SolucaoOperacional & s);
-
-   NSIterator & getIterator(SolucaoOperacional & s);
-
+   Move & move( SolucaoOperacional & );
+   NSIterator & getIterator( SolucaoOperacional & );
    void print();
 
+private:
+   ProblemData & problemData;
+   std::pair< Aula *, Aula * > pickTwoClasses( SolucaoOperacional & );
+   MoveSwapEqBlocksValidator * moveValidator;
 };
 
 #endif /*_NS_SEQ_SWAP_EQ_BLOCKS_H_*/
