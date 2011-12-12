b69a7178013b589133378bb6f29ab04e8de6814f
Francisco Fonseca <ffonseca@gapso.com.br>
2011-12-12 12:17:19 +0000

    TRIEDA-1122: Ao tentar adicionar um aluno a uma demanda, a lista de alunos que deveria aparecer listando as opcoes contem somente "()" para todos os alunos. No campo Periodo, nao eh listada nenhuma opcao. E ao clicar em Salvar, eh exibida a mens "deu falha na conexao"

--- code/client/domain/src/main/java/com/gapso/trieda/domain/Curriculo.java
+++ code/client/domain/src/main/java/com/gapso/trieda/domain/Curriculo.java
@@ -472,7 +472,7 @@ public class Curriculo
     }
 
     @SuppressWarnings( "unchecked" )
-	public int getPeriodo(
+	public Integer getPeriodo(
     	InstituicaoEnsino instituicaoEnsino,
     	Disciplina disciplina, Oferta oferta )
     {
