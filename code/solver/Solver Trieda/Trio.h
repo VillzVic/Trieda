3a1a8301d2ba375343f9b4db88aefe81ea3bc03c
Nara Torres Moreira <nara.moreira@gapso.com.br>
2012-01-25 12:39:03 +0000

    Nova classe usada para tratar a restrição de máximo de creditos em sala, quando há mais de 1 semana letiva

---
+++ code/solver/Solver Trieda/Trio.h
@@ -0,0 +1,51 @@
+#pragma once
+
+template<class T1, class T2, class T3 > 
+class Trio
+{
+public:
+
+	T1 first;
+	T2 second;
+	T3 third;
+
+	Trio(void)
+	{
+	}
+
+	~Trio(void)
+	{
+	}
+
+	void set( T1 t1, T2 t2, T3 t3 ){		
+		first = t1;
+		second = t2;
+		third = t3;
+	}
+	
+   bool operator < ( Trio const & right ) const
+   {
+	  if ( first < right.first &&
+		   second < right.second &&
+		   third < right.third )
+		   return true;
+
+	  if ( first != right.first )
+		  return first < right.first;
+	  if ( second != right.second )
+		  return second < right.second;
+	  if ( third != right.third )
+		  return third < right.third;
+
+	   return false;
+   }
+   
+   bool operator == ( Trio & right ) const
+   { 
+		return (  first == right.first &&
+				  second == right.second &&
+				  third == right.third  );
+   }
+
+};
+
