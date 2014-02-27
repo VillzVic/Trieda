d2fe08746a3b48b973a850b85be14b3ad418e896
Karen Schvartzman <karen@gapso.com.br>
2014-02-27 17:58:03 +0000

    Inserção de lock nos arquivos de exportação.

---
+++ code/client/web/src/main/resources/AtualizarValoresReceita.sql
@@ -0,0 +1,4 @@
+﻿UPDATE campi SET cam_valor_credito=58.63 WHERE cam_id=21;
+UPDATE campi SET cam_valor_credito=59.62 WHERE cam_id=22;
+UPDATE ofertas SET ofe_receita=15.84 WHERE cam_id=21;
+UPDATE ofertas SET ofe_receita=17.18 WHERE cam_id=22;
