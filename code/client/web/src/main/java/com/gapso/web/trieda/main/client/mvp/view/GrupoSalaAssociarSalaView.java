9836fcd49c8780deba376dc0b4edce17fd4a80ac
Henrique Antunes <henrique.antunes@gapso.com.br>
2014-03-06 19:58:34 +0000

    TRIEDA-1790 - Adicionando explicação para cada passo da tela de associar salas a grupo de sala.

--- code/client/web/src/main/java/com/gapso/web/trieda/main/client/mvp/view/GrupoSalaAssociarSalaView.java
+++ code/client/web/src/main/java/com/gapso/web/trieda/main/client/mvp/view/GrupoSalaAssociarSalaView.java
@@ -13,9 +13,11 @@ import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.form.FormButtonBinding;
 import com.extjs.gxt.ui.client.widget.form.FormPanel;
 import com.extjs.gxt.ui.client.widget.form.FormPanel.LabelAlign;
+import com.extjs.gxt.ui.client.widget.form.LabelField;
 import com.extjs.gxt.ui.client.widget.form.TextField;
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
 import com.extjs.gxt.ui.client.widget.layout.FormData;
+import com.extjs.gxt.ui.client.widget.layout.FormLayout;
 import com.extjs.gxt.ui.client.widget.layout.HBoxLayout;
 import com.extjs.gxt.ui.client.widget.layout.HBoxLayout.HBoxLayoutAlign;
 import com.extjs.gxt.ui.client.widget.layout.HBoxLayoutData;
@@ -55,7 +57,7 @@ public class GrupoSalaAssociarSalaView extends MyComposite implements
 	private void initUI() {
 		simpleModal = new SimpleModal("Associar Sala ao Grupo",
 				Resources.DEFAULTS.sala16());
-		simpleModal.setHeight(500);
+		simpleModal.setHeight(520);
 		simpleModal.setWidth(685);
 	}
 
@@ -94,15 +96,30 @@ public class GrupoSalaAssociarSalaView extends MyComposite implements
 
 		contentPanel.add(formPanel);
 
+		LabelField leftLabel = new LabelField();
+		leftLabel.setValue("Selecione um andar e clique em \">\"");
+		leftLabel.addStyleName("associarSalaGrupoLeftLabel");
+		
+		LabelField centerLabel = new LabelField();
+		centerLabel.setValue("Selecione um ou mais ambientes e clique em \"<\" para selecioná-lo(s) e associá-lo(s) ao grupo");
+		centerLabel.addStyleName("associarSalaGrupoCenterLabel");
+		
+		LabelField rightLabel = new LabelField();
+		rightLabel.setValue("Para remover a associação de um ambiente a este grupo, marque e ambiente e clique em \">\"");
+		rightLabel.addStyleName("associarSalaGrupoRightLabel");
+		
 		panelLists = new ContentPanel();
 		panelLists.setBodyBorder(false);
 		panelLists.setHeaderVisible(false);
 		HBoxLayout layout = new HBoxLayout();
 		layout.setHBoxLayoutAlign(HBoxLayoutAlign.TOP);
-		layout.setPadding(new Padding(5));
+		layout.setPadding(new Padding(-25, 5, 5, 5));
 		panelLists.setLayout(layout);
-		panelLists.setHeight(330);
+		panelLists.setHeight(380);
 
+		LayoutContainer left = new LayoutContainer(new FormLayout(LabelAlign.TOP));
+		left.add(leftLabel);
+		left.setWidth(95);
 		ContentPanel andaresListPanel = new ContentPanel(new FitLayout());
 		andaresListPanel.setWidth(95);
 		andaresListPanel.setHeight(320);
@@ -112,11 +129,14 @@ public class GrupoSalaAssociarSalaView extends MyComposite implements
 		andaresList = new ListView<SalaDTO>(store);
 		andaresList.setDisplayProperty(SalaDTO.PROPERTY_ANDAR);
 		andaresListPanel.add(andaresList);
+		left.add(andaresListPanel);
 
+		LayoutContainer center = new LayoutContainer(new FormLayout(LabelAlign.TOP));
+		center.add(centerLabel);
 		ContentPanel naoAssociadasListPanel = new ContentPanel(new FitLayout());
 		naoAssociadasListPanel.setWidth(240);
 		naoAssociadasListPanel.setHeight(320);
-		naoAssociadasListPanel.setHeadingHtml("Sala(s) não associadas ao Grupo");
+		naoAssociadasListPanel.setHeadingHtml("Ambiente(s) não associados ao Grupo");
 		store = new ListStore<SalaDTO>();
 		store.setDefaultSort(SalaDTO.PROPERTY_CODIGO, SortDir.ASC);
 		salasNaoPertencesList = new ListView<SalaDTO>(store);
@@ -125,26 +145,30 @@ public class GrupoSalaAssociarSalaView extends MyComposite implements
 		salasNaoPertencesList.setTemplate("<tpl for=\".\"><div class='x-view-item' " + aria + ">{" + SalaDTO.PROPERTY_CODIGO 
 				+ "}({" + SalaDTO.PROPERTY_NUMERO + "} - {" + SalaDTO.PROPERTY_TIPO_STRING  + "} - {" + SalaDTO.PROPERTY_DESCRICAO + "})" + "</div></tpl>");
 		naoAssociadasListPanel.add(salasNaoPertencesList);
+		center.add(naoAssociadasListPanel);
 
+		LayoutContainer right = new LayoutContainer(new FormLayout(LabelAlign.TOP));
+		right.add(rightLabel);
 		ContentPanel associadasListPanel = new ContentPanel(new FitLayout());
 		associadasListPanel.setWidth(240);
 		associadasListPanel.setHeight(320);
-		associadasListPanel.setHeadingHtml("Sala(s) associadas ao Grupo");
+		associadasListPanel.setHeadingHtml("Ambiente(s) associados ao Grupo");
 		store = new ListStore<SalaDTO>();
 		store.setDefaultSort(SalaDTO.PROPERTY_CODIGO, SortDir.ASC);
 		salasPertencesList = new ListView<SalaDTO>(store);
 		salasPertencesList.setDisplayProperty(SalaDTO.PROPERTY_CODIGO);
 		associadasListPanel.add(salasPertencesList);
+		right.add(associadasListPanel);
 
-		panelLists.add(andaresListPanel, new HBoxLayoutData(new Margins(0, 0,
+		panelLists.add(left, new HBoxLayoutData(new Margins(0, 0,
 				0, 0)));
 		panelLists.add(getAtualizaSalasDoAndarButtonsPanel(),
 				new HBoxLayoutData(new Margins(0, 5, 0, 5)));
-		panelLists.add(naoAssociadasListPanel, new HBoxLayoutData(new Margins(
+		panelLists.add(center, new HBoxLayoutData(new Margins(
 				0, 0, 0, 0)));
 		panelLists.add(getAtualizaSalasButtonsPanel(), new HBoxLayoutData(
 				new Margins(0, 5, 0, 5)));
-		panelLists.add(associadasListPanel, new HBoxLayoutData(new Margins(0,
+		panelLists.add(right, new HBoxLayoutData(new Margins(0,
 				0, 0, 0)));
 
 		contentPanel.add(panelLists);
@@ -156,7 +180,7 @@ public class GrupoSalaAssociarSalaView extends MyComposite implements
 		ContentPanel panel = new ContentPanel();
 		panel.setHeaderVisible(false);
 		panel.setBodyBorder(false);
-		panel.setBodyStyle("display: table-cell; vertical-align: middle");
+		panel.setBodyStyle("display: table-cell; vertical-align: middle; top: 51px");
 		panel.setLayout(new RowLayout(Orientation.VERTICAL));
 
 		atualizaSalasDoAndarBT = new Button();
@@ -176,7 +200,7 @@ public class GrupoSalaAssociarSalaView extends MyComposite implements
 		ContentPanel panel = new ContentPanel();
 		panel.setHeaderVisible(false);
 		panel.setBodyBorder(false);
-		panel.setBodyStyle("display: table-cell; vertical-align: middle");
+		panel.setBodyStyle("display: table-cell; vertical-align: middle; top: 51px");
 		panel.setLayout(new RowLayout(Orientation.VERTICAL));
 
 		adicionaSalasBT = new Button();
