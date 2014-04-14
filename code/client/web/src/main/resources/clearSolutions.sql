bb802535b7155071812c37139dab229453805966
Karen Schvartzman <karen@gapso.com.br>
2014-04-14 16:46:54 +0000

    Update do clear solutions para englobar o atendimento_operacional_motivos_uso.

--- code/client/web/src/main/resources/clearSolutions.sql
+++ code/client/web/src/main/resources/clearSolutions.sql
@@ -1,5 +1,6 @@
-UPDATE alunos_demanda SET ald_atendido = b'0';
+﻿UPDATE alunos_demanda SET ald_atendido = b'0';
 DELETE FROM alunos_demanda_atendimentos_operacional;
 DELETE FROM alunos_demanda_atendimentos_tatico;
 DELETE FROM atendimento_tatico;
+DELETE FROM atendimento_operacional_motivos_uso;
 DELETE FROM atendimento_operacional;
