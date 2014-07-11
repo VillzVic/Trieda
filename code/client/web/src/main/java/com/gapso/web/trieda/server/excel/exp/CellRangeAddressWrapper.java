48adb33fc99225308cd172d5b6ddb57c3bc437a0
Henrique Antunes <henrique.antunes@gapso.com.br>
2014-07-11 17:55:21 +0000

    TRIEDA-1941 - Classe de utilidade para copiar estilo.

---
+++ code/client/web/src/main/java/com/gapso/web/trieda/server/excel/exp/CellRangeAddressWrapper.java
@@ -0,0 +1,24 @@
+package com.gapso.web.trieda.server.excel.exp;
+
+import org.apache.poi.ss.util.CellRangeAddress;
+
+public class CellRangeAddressWrapper implements Comparable<CellRangeAddressWrapper> {  
+	   
+    public CellRangeAddress range;  
+      
+    public CellRangeAddressWrapper(CellRangeAddress theRange) {  
+          this.range = theRange;  
+    }  
+      
+    public int compareTo(CellRangeAddressWrapper o) {  
+    	if (range.getFirstColumn() < o.range.getFirstColumn()  
+    			&& range.getFirstRow() < o.range.getFirstRow()) {  
+    		return -1;  
+    	} else if (range.getFirstColumn() == o.range.getFirstColumn()  
+    			&& range.getFirstRow() == o.range.getFirstRow()) {  
+    		return 0;  
+    	} else {  
+    		return 1;  
+    	}  
+    }  
+}  
